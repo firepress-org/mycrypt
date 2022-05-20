ec4c3afa70abc7ac120ff85e72e7151f509990be
Pascal Andy <pascalandy@users.noreply.github.com>
2022-05-20 08:55:31 -0400

    Dummy Commit 1 - 6e50

--- .bl_override.sh
+++ .bl_override.sh
@@ -1,13 +1,15 @@
 #!/usr/bin/env bash
 
-### For YOUR projects, .bl_override.sh must be part of YOUR project
-
+### =>         Override vars from `.bl_override.sh`
 APP_NAME="mycrypt"
 GITHUB_USER="firepress-org"
 APP_VERSION="0.1.80"
 
+### manage your custom scripts under this directory /private/**.sh
 CFG_USE_PRIVATE_DIRECTORY="true"
+
+### function options
 CFG_USE_GPG_SIGNATURE="true"
 
-### BRANCH NAMES
+### branch names
 CFG_DEFAULT_BRANCH="master"
