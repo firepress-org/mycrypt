a90f6f749dea4302a49691a5823e721118f464a0
Pascal Andy <pascalandy@users.noreply.github.com>
2022-05-25 16:43:37 -0400

    Update mycrypt to version 0.1.84

--- .bl_override.sh
+++ .bl_override.sh
@@ -3,7 +3,7 @@
 
 APP_NAME="mycrypt"
 GITHUB_USER="firepress-org"
-APP_VERSION="0.1.83"
+APP_VERSION="0.1.84"
 
 ### manage your custom scripts under this directory /private/**.sh
 CFG_USE_PRIVATE_DIRECTORY="true"
